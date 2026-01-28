c24035614eb287503ce3832dc1014c89b1d6cf92
Nastrembicke <Nastrembicke@crimson.ua.edu>
2026-01-27 19:34:19 -0600

    Lab 2 submission

---
+++ app.py
@@ -0,0 +1,4 @@
+first_grade = float(input("What did you make on the first exam?"))
+second_grade = float(input("What did you make on the second exam?"))
+
+print(f" The average of the two exams was: {(first_grade + second_grade) / 2}")
